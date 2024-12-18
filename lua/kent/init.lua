6a3c0c40cf22a995305ae4ce55097c473b6adc41
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2024-12-19 06:23:52 +0800

    ADD: more configs to the lua file

--- lua/kent/init.lua
+++ lua/kent/init.lua
@@ -7,3 +7,18 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8 -- Do not go down/up without at least 8 chars from the bottom
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = '80' -- Set a 80 chars boundary
