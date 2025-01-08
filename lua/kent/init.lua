87d51c1c8e6d2f088006b1f90c1f28fc47c82e44
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2025-01-09 05:39:48 +0800

    fix and new thing

--- lua/kent/init.lua
+++ lua/kent/init.lua
@@ -10,14 +10,13 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8 -- Do not go down/up without at least 8 chars from the bottom
 
 vim.opt.updatetime = 50
 
